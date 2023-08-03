c8a866b635d456dd24e6b467ede8c431b0d4319f
Atulyw <atulyw@greamio.com>
2023-08-03 19:35:31 +0530

    atulyw| galaxy-service added with springboot

---
+++ src/main/java/com/cloudblitz/galaxyservice/GalaxyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cloudblitz.galaxyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaxyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaxyServiceApplication.class, args);
+	}
+
+}
